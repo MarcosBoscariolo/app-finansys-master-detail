27ad87ebaf1ab9d14c9257c84a12882020d8687a
MarcosBoscariolo <marcosboscariolo@hotmail.com>
2023-06-26 21:36:12 -0300

    adding EntryFormComponent

---
+++ src/app/pages/entries/entry-form/entry-form.component.ts
@@ -0,0 +1,131 @@
+import { Component, OnInit, AfterContentChecked } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { switchMap } from 'rxjs';
+import { ToastrService } from 'ngx-toastr';
+
+import { EntryService } from './../shared/service/entry.service';
+import { Entry } from '../shared/model/entry.class';
+
+@Component({
+  selector: 'app-entry-form',
+  templateUrl: './entry-form.component.html',
+  styleUrls: ['./entry-form.component.css']
+})
+export class EntryFormComponent implements OnInit, AfterContentChecked {
+
+  currentAction: string;
+  pageTitle: string;
+  entryForm: FormGroup;
+  submittingForm: boolean = false;
+  serverErrorMessages: string[] | null = null;
+  entry: Entry = new Entry();
+
+  constructor(
+    private entryService:EntryService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private formBuilder: FormBuilder,
+    private toastr: ToastrService
+  ) { }
+
+  ngOnInit(): void {
+    this.setCurrentAction();
+    this.buildEntryForm();
+    this.loadEntry();
+  }
+
+  ngAfterContentChecked(): void {
+    this.setPageTitle();
+  }
+
+  submitForm() {
+    this.submittingForm = true;
+    if (this.currentAction == "new") {
+      this.createEntry();
+      return;
+    }
+    this.updateEntry();
+  }
+
+  private setCurrentAction() {
+   if (this.activatedRoute.snapshot.url[0].path == "new") {
+    this.currentAction = "new";
+   } else {
+    this.currentAction = "edit";
+   }
+  }
+
+  private buildEntryForm() {
+    this.entryForm = this.formBuilder.group({
+      id: [null],
+      name: [null, [Validators.required, Validators.minLength(2)]],
+      description: [null],
+      type: [null, [Validators.required]],
+      amount: [null, [Validators.required]],
+      date: [null, [Validators.required]],
+      paid: [null, [Validators.required]],
+      categoryId: [null, [Validators.required]],
+    });
+  }
+
+  private loadEntry() {
+    if (this.currentAction != "edit") {
+      return;
+    }
+
+    this.activatedRoute.paramMap.pipe(
+      switchMap(entryParams => this.entryService.getById(Number(entryParams.get("id"))))
+    ).subscribe({
+      next: (entry) => {
+        this.entry = entry;
+        this.entryForm.patchValue(this.entry);
+      },
+      error: () => alert("Ops... Ocorreu um erro interno, tente novamente!")
+    });
+  }
+
+  private setPageTitle() {
+    if (this.currentAction == "new") {
+      this.pageTitle = 'Cadastro de novo lançamento';
+      return;
+    }
+    const entryName = this.entry.name || "";
+    this.pageTitle = 'Editando lançamento: ' + entryName;
+  }
+
+  private createEntry() {
+    const entry: Entry = Object.assign(new Entry(), this.entryForm.value);
+    this.entryService.create(entry).subscribe({
+      next: (entry) => this.actionsForSucces(entry),
+      error: (error) => this.actionsForError(error)
+    });
+  }
+
+  private updateEntry() {
+    const entry: Entry = Object.assign(new Entry(), this.entryForm.value);
+    this.entryService.update(entry).subscribe({
+      next: (entry) => this.actionsForSucces(entry),
+      error: (error) => this.actionsForError(error)
+    });
+  }
+
+  private actionsForSucces(entry: Entry){
+    this.toastr.success("Solicitação processada com sucesso!");
+    this.router.navigateByUrl('entries', {skipLocationChange: true}).then(
+      () => this.router.navigate(['entries', 'edit', entry.id])
+    );
+  }
+
+  private actionsForError(error: any) {
+    this.toastr.error('Ops... Ocorreu um erro ao processar sua solicitação!');
+    this.submittingForm = false;
+    if (error.status === 422) {
+      this.serverErrorMessages = JSON.parse(error.body).errors;
+    } else {
+      this.serverErrorMessages = ['Falha na comunicação com o servidor. Por favor, tente mais tarde.'];
+    }
+  }
+
+}
